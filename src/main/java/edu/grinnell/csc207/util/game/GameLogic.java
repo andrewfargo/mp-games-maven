af30b5b58f40628909b3f4ffd29b2559daf677c6
khanhdo05 <khanhphuongdo28@gmail.com>
2024-11-04 20:22:38 -0600

    simple set up

--- src/main/java/edu/grinnell/csc207/util/game/GameLogic.java
+++ src/main/java/edu/grinnell/csc207/util/game/GameLogic.java
@@ -1,1 +1,73 @@
 package edu.grinnell.csc207.util.game;
+
+import edu.grinnell.csc207.util.matrix.Matrix;
+import edu.grinnell.csc207.util.matrix.MatrixV0;
+
+/**
+ * Core game logic for the game of Wordle.
+ *
+ * @author Khanh Do
+ * @author Andrew Fargo
+ */
+public class GameLogic {
+  // +-----------+---------------------------------------------------
+  // | Constants |
+  // +-----------+
+
+  /**
+   * The default width.
+   */
+  static final int DEFAULT_WIDTH = 5;
+
+  /**
+   * The default number of rows.
+   */
+  static final int DEFAULT_HEIGHT = 6;
+
+  // +--------+---------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The number of guesses the player has left.
+   */
+  private int guessLeft = DEFAULT_HEIGHT;
+
+  /**
+   * The current state of the board.
+   */
+  private Matrix<String> board;
+
+  /**
+   * The target word.
+   */
+  private Words targetWord;
+
+  // +-------------+---------------------------------------------------
+  // | Constructor |
+  // +-------------+
+
+  /**
+   * Create a new game.
+   */
+  public GameLogic() {
+    this.board = new MatrixV0<>(GameLogic.DEFAULT_WIDTH, GameLogic.DEFAULT_HEIGHT, "");
+
+    // TODO: Use Words class to get word for this game
+    this.targetWord = new Words();
+  } // GameLogic()
+
+  // +---------+---------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  /**
+   * toString
+   */
+
+  /**
+   * registerGuess
+   */
+
+
+} // class GameLogic
--- src/main/java/edu/grinnell/csc207/util/game/Words.java
+++ src/main/java/edu/grinnell/csc207/util/game/Words.java
@@ -1,1 +1,17 @@
 package edu.grinnell.csc207.util.game;
+
+public class Words {
+  public Words() {
+
+  } // Words()
+
+  // getRandomWord()
+  public String getRandomWord() {
+    return "hello";
+  } // getRandomWord()
+
+  // checkValidEnglishWord()
+  public boolean checkValidEnglishWord(String word) {
+    return true;
+  } // checkValidEnglishWord()
+} // class Words
