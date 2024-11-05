85c2b2a097da29a0131fe00d49be8c2fb2065c24
Drew Fargo <drew.fargo@gmail.com>
2024-11-05 01:07:11 -0600

    What can only be described as a 1am binary search on a file

--- src/main/java/edu/grinnell/csc207/util/game/Words.java
+++ src/main/java/edu/grinnell/csc207/util/game/Words.java
@@ -1,5 +1,8 @@
 package edu.grinnell.csc207.util.game;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  * Describes the wordlist used by the game logic.
  * @author Andrew Fargo
@@ -9,17 +12,27 @@ public class Words {
    * The list of words the game will choose from.
    * Must be a subset of wordlist.
    */
-  private File wordlist;
+  private Path wordlist;
 
   /**
    * The list of words the game considers valid.
    * Must be a superset of wordlist.
    */
-  private File checklist;
+  private Path checklist;
+
+  /* I'm choosing to store the following redundantly,
+     this makes file calculations much easier, but requires
+     initialization at runtime. */
   
-  /** How long each word is. */
+  /** How long each word is in characters. */
   private int length;
 
+  /** How many words exist in the wordlist. */
+  private int wordlistLen;
+  
+  /** How many words exist in the checklist. */
+  private int checklistLen;
+  
   public Words(String wordlistPath) {
     this(wordlistPath, wordlistPath);
   } // Words(String)
@@ -43,7 +56,25 @@ public class Words {
    * @return true if it is present, false otherwise.
    */
   public boolean isValidWord(String word) {
-    throw new NotImplementedException();
+    int lineUB = 0;
+    int lineLB = checklistLen;
+    int lineMB; 
+    BufferedReader clReader = Files.newBufferedReader(checklist);
+    clReader.mark();
+    while (lineUB > lineLB) {
+      clReader.reset();
+      lineMB = lineLB + (lineUB - lineLB) / 2;
+      clReader.skip(lineMB * (this.length + 1)); // add for newline
+      int cmp = word.compareTo(clReader.readline());
+      if (cmp > 0) {
+	lineLB = lineMB + 1;
+      } else if (cmp < 0) {
+	lineUB = lineLB;
+      } else {
+	return true;
+      } // if/else
+    } // while
+    return false;
   } // isValidWord(String)
 
   /**
