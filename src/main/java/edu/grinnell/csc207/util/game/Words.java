31d6607778a079ae82eecd380058a2090a1f9ce2
Drew Fargo <drew.fargo@gmail.com>
2024-11-05 17:58:49 -0600

    Add (currently untested) Words implementation

--- src/main/java/edu/grinnell/csc207/util/game/Words.java
+++ src/main/java/edu/grinnell/csc207/util/game/Words.java
@@ -2,12 +2,21 @@ package edu.grinnell.csc207.util.game;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.IntStream;
+import java.io.IOException;
+import java.util.Random;
+import java.io.BufferedReader;
 
 /**
- * Describes the wordlist used by the game logic.
+ * Manages operations related to producing and validating words.
+ * An unordered list of words available as answers (wordlist),
+ * and valid guesses (checklist) are provided.
+ * As of Nov 5, 2024, wordlist is bounded by INT_MAX words,
+ * but checklist may be arbitrarily large according to system limits.
  * @author Andrew Fargo
  */
 public class Words {
+  private static final int WORDLEN_LIMIT = 64;
   /**
    * The list of words the game will choose from.
    * Must be a subset of wordlist.
@@ -29,52 +38,109 @@ public class Words {
 
   /** How many words exist in the wordlist. */
   private int wordlistLen;
+
+  /** Word index permutation. */
+  private int[] indices;
+
+  /** Current index. */
+  private int index;
+
+  /**
+   * Ensures every line in file specified is of length this.length.
+   * @param path The file.
+   * @throws IOException If this is not the case.
+   */
+  private void validateLengths(Path path) throws IOException {
+    if (Files.lines(path).parallel().anyMatch((line) -> line.length() != this.length)) {
+      throw new IOException("File " + path + " has inconsistent sizes.");
+    } // if
+  } // validateLengths(BufferedReader)
+
+  /**
+   * Initializes this.indices to an array of wordlistLen values.
+   * This array is then shuffled to provide a permutation of words
+   * delivered to the player.
+   * @param seed The random seed from the save file.
+   */
+  private void generateIndices(long seed) {
+    Random rng = new Random(seed);
+    this.indices = IntStream.range(0, wordlistLen).toArray();
+    // Fisher-Yates from Knuth TAOCP v.2 Algorithm P (Shuffling)
+    for (int i = wordlistLen - 1; i > 0; i--) {
+      int j = rng.nextInt(i);
+      int temp = this.indices[i];
+      indices[i] = indices[j];
+      indices[j] = temp;
+    } // for
+  } // generateIndices(int)
+
   
-  /** How many words exist in the checklist. */
-  private int checklistLen;
-  
-  public Words(String wordlistPath) {
-    this(wordlistPath, wordlistPath);
+  /**
+   * Initializes Words with just a wordlist. Assumes checklist is wordlist.
+   * @param wordlistPath The path string to the wordlist.
+   * @param seed The random seed according to the save file.
+   * @param numGames The number of games played by the save file.
+   */
+  public Words(String wordlistPath, long seed, int numGames) throws IOException {
+    this(wordlistPath, wordlistPath, seed, numGames);
   } // Words(String)
 
-  public Words(String wordlistPath, String checklistPath) {
-    throw new NotImplementedException();
+  /**
+   * Initializes Words with a wordlist and a checklist.
+   * @param wordlistPath The path string to the wordlist file.
+   * @param checklistPath The path string to the checklist file.
+   * @param seed The random seed according to the savefile.
+   * @param numGames The number of games played by the save file.
+   */
+  public Words(String wordlistPath, String checklistPath, long seed, int numGames) throws IOException {
+    // Initialize the paths
+    // ====================
+    this.wordlist = Path.of(wordlistPath);
+    this.checklist = Path.of(checklistPath);
+
+    // Validate the files
+    // ==================
+    
+    BufferedReader wordReader = Files.newBufferedReader(this.wordlist);
+    wordReader.mark(Words.WORDLEN_LIMIT);
+    this.length = wordReader.readLine().length();
+    wordReader.reset();
+    wordReader.close();
+
+    this.validateLengths(this.wordlist);
+    this.validateLengths(this.checklist);
+
+    this.wordlistLen = (int) Files.lines(this.wordlist)
+      .limit(Integer.MAX_VALUE).parallel().count();
+    
+    // Generate an index permutation
+    // =============================
+
+    this.generateIndices(seed);
+    this.index = numGames;
   } // Words(String, String)
   
   /**
-   * Gets a random word.
-   * @return A random word from the wordlist, exactly in the
+   * Gets the next word.
+   * @return A pseudorandom word from the wordlist, exactly in the
    *    form specified by the list.
    */
-  public String getRandomWord() {
-    throw new NotImplementedException();
+  public String getNextWord() throws IOException {
+    BufferedReader wlReader = Files.newBufferedReader(wordlist);
+    int wordno = this.indices[this.index++];
+    wlReader.skip((this.length + 1) * wordno);
+    String ret = wlReader.readLine();
+    wlReader.close();
+    return ret;
   } // getRandomWord()
 
   /**
-   * Checks if a word is present in the checklist.
+   * Checks if a word is present in the checklist; conducts a parallelized linear search.
    * @param word The word to check.
    * @return true if it is present, false otherwise.
    */
-  public boolean isValidWord(String word) {
-    int lineUB = 0;
-    int lineLB = checklistLen;
-    int lineMB; 
-    BufferedReader clReader = Files.newBufferedReader(checklist);
-    clReader.mark();
-    while (lineUB > lineLB) {
-      clReader.reset();
-      lineMB = lineLB + (lineUB - lineLB) / 2;
-      clReader.skip(lineMB * (this.length + 1)); // add for newline
-      int cmp = word.compareTo(clReader.readline());
-      if (cmp > 0) {
-	lineLB = lineMB + 1;
-      } else if (cmp < 0) {
-	lineUB = lineLB;
-      } else {
-	return true;
-      } // if/else
-    } // while
-    return false;
+  public boolean isValidWord(String word) throws IOException {
+    return Files.lines(this.checklist).parallel().findAny().isPresent();
   } // isValidWord(String)
 
   /**
