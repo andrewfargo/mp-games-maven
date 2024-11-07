6733eb44c07e2c67dcd7a91f62fb749694a4a731
Drew Fargo <drew.fargo@gmail.com>
2024-11-06 23:59:03 -0600

    Implement file operations to Scores

--- src/main/java/edu/grinnell/csc207/util/game/Scores.java
+++ src/main/java/edu/grinnell/csc207/util/game/Scores.java
@@ -3,71 +3,75 @@ package edu.grinnell.csc207.util.game;
 import java.util.HashMap;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Map;
 import java.util.function.Function;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Scores {
-  /** Default save path. */
-  private static final Path SAVE_DEFAULT = Path.of(".", "scores.dat");
-  
   /** The storage data. Score to frequency. */
   private Map<Integer, Integer> histogram;
 
   /** Path of the save file. */
   private Path path;
 
-  /** Create an empty save file with default path. */
-  public Scores() {
-    this(SAVE_DEFAULT);
-  } // Scores()
-
   /**
    * Create or read from a save file.
    * @param saveFile The save file to be created or read from.
    */
-  public Scores(Path saveFile) {
+  public Scores(Path saveFile) throws IOException {
     this.histogram = new HashMap<Integer, Integer>();
     this.path = saveFile;
-    this.load();
+    if (Files.exists(saveFile)) {
+      this.load();
+    } else {
+      this.save(); // Necessarily empty, creates
+    } // if/else
   } // Scores(Path)
 
   /**
    * Save the current values to disk.
    */
-  public void save() {
-
-  }
+  public void save() throws IOException {
+    Files.writeString(this.path, this.toString(), StandardOpenOption.WRITE,
+		      StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+  } // save()
 
   /**
    * Load path into current values.
    */
-  private void load() {
-
-  }
-
-  /**
-   * From the string formatted by toString, generate a map.
-   * @param String A string of format specified by toString()
-   * @return The map.
-   */
-  private static Map<Integer, Integer> fromString(String data) {
-    return null;
-  }
-
+  private void load() throws IOException {
+    this.histogram.clear();
+    Scanner sc = new Scanner(this.path);
+    while (sc.hasNextInt()) {
+      try {
+	int guesses = sc.nextInt();
+	int freq = sc.nextInt();
+	this.histogram.put(guesses, freq);
+      } catch (Exception e) {
+	sc.close();
+	throw new IOException("Save file corrupt when loading: " + e.getMessage());
+      } // try/catch
+    } // while
+    sc.close();
+  } // load()
+  
   /**
    * Represents the map as a human-readable and computer-readable string.
    * @return The String representation.
    */
+  @Override
   public String toString() {
     Function<Map.Entry<Integer, Integer>, String> toFormat = (e) ->
       "" + e.getKey() + ": " + e.getValue() + "\n";
     
     return this.histogram.entrySet().stream()
       .sorted(Map.Entry.comparingByKey())
-      .parallel().map(toFormat).reduce((s1, s2) -> s1.concat(s2)).get();
+      .parallel().map(toFormat).reduce("", (s1, s2) -> s1.concat(s2));
   } // toString()
 
   public void add(int score) {
-    
+    this.histogram.compute(score, (k, v) -> (v == null) ? 1 : v + 1);
   } // add(int)
 } // class Scores
